ef53e817de84b8d9fd12a4c705981223f77a4ede
gornin <gnkevin@163.com>
2023-03-29 19:40:04 +0800

    feat: 简繁转换

--- README.md
+++ README.md
@@ -1,5 +1,51 @@
-# 汉字转拼字
+# 汉字处理助手
 
-`中文` -> `ZHONG_WEN`
+author: [gornin](https://github.com/gornin)
 
-时间紧任务重，先满足自己的基本使用，后面会迭代完善的。
+初衷：最近在做公司的一个国际化项目，简繁外语等字体的locale配置太恶心繁琐了。能提高一点效率，也算拯救自己于水火。
+
+感谢：vscode、juejin社区、market里的vscode插件
+
+## 支持快捷键
+
+`u`—— `uppercase` 大写
+`l`—— `lowercase` 小写
+`f`—— `traditional` 繁体
+`j`—— `simplified` 简体
+
+```json
+"keybindings": [
+    {
+    "command": "汉字转大写拼音",
+    "key": "ctrl+shift+alt+u",
+    "mac": "ctrl+alt+cmd+u",
+    },
+    {
+    "command": "汉字转小写拼音",
+    "key": "ctrl+shift+alt+l",
+    "mac": "ctrl+alt+cmd+l",
+    },
+    {
+    "command": "简体转繁体",
+    "key": "ctrl+shift+alt+f",
+    "mac": "ctrl+alt+cmd+f",
+    },
+    {
+    "command": "繁体转简体",
+    "key": "ctrl+shift+alt+j",
+    "mac": "ctrl+alt+cmd+j",
+    }
+],
+```
+
+### 支持右键菜单
+```json
+"menus": {
+    "editor/context": [
+        "汉字->拼音大",
+        "汉字->拼音小",
+        "简->繁",
+        "繁->简",
+    ]
+}
+```
--- src/extension.ts
+++ src/extension.ts
@@ -2,48 +2,132 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import pinyin from "pinyin";
+import transverter, { OPTIONS } from "./transverter";
+import { IPinyinMode, IPinyinOptions, IPinyinStyle } from "pinyin/lib/declare";
+
+/**
+ * 处理简繁转换
+ *
+ * @param textEditor
+ * @param options
+ */
+function tsProcess(textEditor: vscode.TextEditor, options: OPTIONS) {
+  const doc = textEditor.document;
+  let selection: vscode.Selection | vscode.Range = textEditor.selection;
+  if (selection.isEmpty) {
+    const start = new vscode.Position(0, 0);
+    const end = new vscode.Position(
+      doc.lineCount - 1,
+      doc.lineAt(doc.lineCount - 1).text.length
+    );
+    selection = new vscode.Range(start, end);
+  }
+  let text = doc.getText(selection);
+  textEditor.edit((builder) => {
+    builder.replace(selection, transverter(text, options));
+  });
+}
+
+/**
+ * 汉字转成拼音形式
+ *
+ * @param options pinyin库的配置项
+ * @param connector 连接符
+ * @param type upper | lower
+ * @returns
+ */
+function transform2Py(
+  options: IPinyinOptions,
+  connector: string,
+  type: string
+) {
+  const activeEditor = vscode.window.activeTextEditor;
+  if (!activeEditor) {
+    return;
+  }
+  const { selection } = activeEditor;
+  const selected = activeEditor.document.getText(selection);
+  const pinyinArr = pinyin(selected, options);
+  let str = pinyinArr.toString().replaceAll(",", connector);
+  if (type === "upper") {
+    str = str.toUpperCase();
+  }
+  activeEditor.edit((editBuilder) => {
+    editBuilder.replace(activeEditor.selection, str);
+  });
+}
+
+const PY_COMMANDS = [
+  {
+    command: "cc2py.chars-2-pinyin-upper",
+    options: {
+      style: "normal" as IPinyinStyle, // 普通风格，即不带声调。
+      mode: "normal" as IPinyinMode,
+      segment: false, // 启用分词 "nodejieba" | "segmentit" | "@node-rs/jieba"
+      group: false, // 启用词组
+      heteronym: false, // 返回多音字
+      compact: false, // 如果设置为 true，则将多音字可能的各种组合排列后返回
+    },
+    connector: "_",
+    type: "upper",
+    func: transform2Py,
+  },
+  {
+    command: "cc2py.chars-2-pinyin-lower",
+    options: {
+      style: "normal" as IPinyinStyle, // 普通风格，即不带声调。
+      mode: "normal" as IPinyinMode,
+      segment: false, // 启用分词 "nodejieba" | "segmentit" | "@node-rs/jieba"
+      group: false, // 启用词组
+      heteronym: false, // 返回多音字
+      compact: false, // 如果设置为 true，则将多音字可能的各种组合排列后返回
+    },
+    connector: "_",
+    type: "lower",
+    func: transform2Py,
+  },
+];
+
+const TS_COMMANDS = [
+  {
+    command: "cc2py.s-2-t",
+    options: { type: "traditional", language: "" },
+  },
+  {
+    command: "cc2py.t-2-s",
+    options: { type: "simplified", language: "" },
+  },
+  {
+    command: "cc2py.s-2-t-tw",
+    options: { type: "traditional", language: "zh_TW" },
+  },
+  {
+    command: "cc2py.t-2-s-tw",
+    options: { type: "simplified", language: "zh_TW" },
+  },
+];
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  // console.log('Congratulations, your extension "helloworld" is now active!');
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  // let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
-  // 	// The code you place here will be executed every time your command is executed
-  // 	// Display a message box to the user
-  // 	vscode.window.showInformationMessage('Hello World from HelloWorld!');
-  // });
-  // context.subscriptions.push(disposable);
+  PY_COMMANDS.forEach((item) => {
+    context.subscriptions.push(
+      vscode.commands.registerCommand(item.command, () =>
+        item.func(item.options, item.connector, item.type)
+      )
+    );
+  });
 
-  let disposable = vscode.commands.registerCommand(
-    "cc2py.executeDefinitionProvider",
-    function () {
-      const activeEditor = vscode.window.activeTextEditor;
-      if (!activeEditor) {
-        return;
-      }
-      const { selection } = activeEditor;
-      const selected = activeEditor.document.getText(selection);
-      const pinyinArr = pinyin(selected, {
-        style: "normal", // 普通风格，即不带声调。
-        mode: "normal",
-        segment: false, // 启用分词 "nodejieba" | "segmentit" | "@node-rs/jieba"
-        group: false, // 启用词组
-        heteronym: false, // 返回多音字
-        compact: false, // 如果设置为 true，则将多音字可能的各种组合排列后返回
-      });
-      //   console.log(pinyinArr);
-      const str = pinyinArr.toString().replaceAll(",", "_").toUpperCase();
-      activeEditor.edit((editBuilder) => {
-        editBuilder.replace(activeEditor.selection, str);
-      });
-    }
-  );
-  context.subscriptions.push(disposable);
+  TS_COMMANDS.forEach((item) => {
+    context.subscriptions.push(
+      vscode.commands.registerTextEditorCommand(
+        item.command,
+        (textEditor: vscode.TextEditor) => {
+          tsProcess(textEditor, item.options);
+        }
+      )
+    );
+  });
 }
 
 // This method is called when your extension is deactivated
